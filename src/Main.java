9f4e2ff36dcc62e8ee277e1303344514e5c92b24
Pawel Wilkolek <pawelwilkolek@gmail.com>
2021-06-13 10:47:07 +0200

    Pierwszyprogram

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("HelloWorld");
+        System.out.println("CześćPaweł");
+    }
+}
